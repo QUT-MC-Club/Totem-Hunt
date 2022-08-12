939c4661a913d6cdd9b3133ba7987d77bfd1da05
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-08-12 14:14:42 -0400

    Update minigame to Minecraft 1.19.1

--- src/main/java/io/github/haykam821/totemhunt/game/role/HolderRole.java
+++ src/main/java/io/github/haykam821/totemhunt/game/role/HolderRole.java
@@ -6,11 +6,10 @@ import java.util.List;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 
 public class HolderRole extends Role {
-	private static final Text NAME = new TranslatableText("text.totemhunt.role.holder").formatted(Formatting.GOLD);
+	private static final Text NAME = Text.translatable("text.totemhunt.role.holder").formatted(Formatting.GOLD);
 	private static final ItemStack TOTEM = new ItemStack(Items.TOTEM_OF_UNDYING);
 
 	@Override
